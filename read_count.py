a4c5ee3e82616604dda71ff7a1f3d4ef222fa151
smilenaxx <smilenaxx@163.com>
2016-12-06 20:06:17 +0800

    amend bug

--- read_count.py
+++ read_count.py
@@ -55,7 +55,7 @@ def ip_open_url(url):
 	opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.1708.400 QQBrowser/9.5.9635.400')]
 
 	urllib.request.install_opener(opener)
-	page = urllib.request.urlopen(targetUrl)
+	page = urllib.request.urlopen(targetUrl,timeout = 10)
 	html = page.read().decode('utf-8')
 	
 	return html
@@ -68,10 +68,10 @@ def get_top_url(top_name):
 	name_real_url =''
 	
 	while 1:
-		name_html = ip_open_url(name_url)#打开url
-		p = re.compile('href="([^"]*)"><span></span><img')
 		
 		try:
+			name_html = ip_open_url(name_url)#打开url
+			p = re.compile('href="([^"]*)"><em><!--red_beg-->' + top_name)
 			#搜索到公众号并成功获取url
 			if p.search(name_html):
 				name_real_url = p.search(name_html).group(1).replace('amp;','')
@@ -83,19 +83,26 @@ def get_top_url(top_name):
 			#在搜索公众号的时候被antispider发现了
 			else:
 				print(top_name + ':antispider---' + 'get_top_url')
-		
+			
 		#此IP不好使，换一个
+		except urllib.error.HTTPError as e:
+			pass
 		except urllib.error.URLError as e:
-			print(e.reason)
+			pass
+		except TimeoutError as e:
+			pass
+		except OSError as e:
+			pass
+		
 		
 	return name_real_url
 
 	
-#获取排行榜前50的数据，返回一个二维列表	
-def get_top(html):
+#获取排行榜需要部分的数据，返回一个二维列表	
+def get_top(html,n1,n2):
 	
 	#需要匹配的REs
-	p1 = re.compile(r'class="name">\w+\D+\d/\d\D+\d+\D+\d+\D+\d+\D+\d+\D+\d+\D+\d+')
+	p1 = re.compile(r'class="name">[^<]+\D+[^<]+\D+[^<]+\D+[^<]+\D+[^<]+\D+[^<]+\D+[^<]+\D+[^<]+')
 	p2 = re.compile(r'</?td>\D*')
 
 	#处理html，得到可用数据
@@ -104,9 +111,7 @@ def get_top(html):
 		top_list[i] = p2.sub('_',top_list[i])
 		top_list[i] = top_list[i].replace('class="name">','')
 	
-	#获取前n名的数据
-	n = 50
-	top_list = top_list[:n]
+	top_list = top_list[n1-1:n2]
 	for i in range(len(top_list)):
 		top_list[i] = top_list[i].split('_')
 	
@@ -176,10 +181,10 @@ def campare_data(list,dict):
 	for i in range(len(fact_data_list)):
 		fact_data_list[i] = fact_data_list[i].split(':')
 		if top_data_dict[fact_data_list[i][0]] == fact_data_list[i][1]:
-			temp_y = '%s:   真实-%s   统计-%s   符合-yes'%(fact_data_list[i][0],fact_data_list[i][1],fact_data_list[i][1])
+			temp_y = '符合-yes   %s:   真实-%s   统计-%s'%(fact_data_list[i][0],fact_data_list[i][1],fact_data_list[i][1])
 			compare_list.append(temp_y)
 		else:
-			temp_n = '%s:   真实-%s   统计-%s   符合-no'%(fact_data_list[i][0],fact_data_list[i][1],top_data_dict[fact_data_list[i][0]])
+			temp_n = '符合-no    %s:   真实-%s   统计-%s'%(fact_data_list[i][0],fact_data_list[i][1],top_data_dict[fact_data_list[i][0]])
 			compare_list.append(temp_n)
 			
 	return compare_list
@@ -202,18 +207,22 @@ def read_main():
 	find_start_str = input('请输入需要查找的结束时间(xxxx-xx-xx):')
 	find_start = time_to_stamp(find_start_str) + 86400
 	
+	#获取前n名的数据
+	n1 = int(input('排行榜开始名次(1-100):'))
+	n2 = int(input('排行榜结束名次(1-100):'))
+	
 	#url
-	url = "http://112.126.82.29/pubranking/week.html?scode=edupress&day=2016-11-14+%E8%87%B3+2016-11-20" #%(find_end_str,find_start_str)
+	url = "http://112.126.82.29/pubranking/week.html?scode=edupress"
 	each_url = ''
 	final_list_ele = ''
 	
 	#得到榜单数据的list和dict
-	top_list = get_top(head_open_url(url))
+	top_list = get_top(head_open_url(url),n1,n2)
 	print('获取排行榜正常')
 	compare_dict = list_to_dict(top_list)
 	
 	final_list = []
-	top_num = 1
+	top_num = n1
 	
 	for name in top_list:
 		each_url = get_top_url(name[0]) #用代理ip能正确读出url
@@ -238,29 +247,32 @@ def read_main():
 		else:
 			print('%s.%s:未搜索到该公众号'%(top_num,name[0]))
 			final_list_ele = '%s:未找到'%(name[0])
-			
-		top_num += 1 #排名
 		
-		final_list.append(final_list_ele) #将每个公众号的数据录入final_list
+		with open('final_university.txt','a') as f:
+			f.write('%s.'%top_num + final_list_ele + '\n')
+		
+		top_num += 1 #排名
 
-		time.sleep(1) #延时1秒
-	
-	with open('final.txt','w') as f:
-		num = 1
-		for each in final_list:
-			f.write('%s.'%num + each + '\n')
-			num += 1
-	print('获取实际数据功能正常,数据保存在与程序相同文件夹,final.txt文件中')
-	
-	#得到比较结果		
-	campare_list = campare_data(final_list,compare_dict)
+		time.sleep(0.5) #延时1秒
 
-	with open('compare.txt','w') as f:
-		num = 1
-		for each in campare_list:
-			f.write('%s.'%num + each + '\n')
-			num += 1
-	print('获取比较数据功能正常,数据保存在与程序相同文件夹,compare.txt文件中')			
+	#为了避免发生异常程序中断，所以从数据完全取得后，再进行比较
+	do_compare = input("是否进行比较(yes/no):")
+	
+	if do_compare == 'yes':
+		with open('final_university.txt','r') as f:
+			for each in f.readlines():
+				each = each.strip()
+				final_list.append(each.split('.')[1])
+		
+		#得到比较结果		
+		campare_list = campare_data(final_list,compare_dict)
+
+		with open('compare_university.txt','w') as f:
+			num = 1
+			for each in campare_list:
+				f.write('%s.'%num + each + '\n')
+				num += 1
+		print('获取比较数据功能正常,数据保存在与程序相同文件夹的compare_university.txt文件中')			
 	
 	print('program over')
 
